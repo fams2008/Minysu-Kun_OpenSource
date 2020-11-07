03ab6c0933d844995ec9b7625e5eacef8d55b932
fams2008 <73937616+fams2008@users.noreply.github.com>
2020-11-07 19:40:42 +0000

    Update email.js

--- commands/email.js
+++ commands/email.js
@@ -2,5 +2,5 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message, args) => {
 
-await message.channel.send(" (usa este comando por exemplo para pedir a ele para te criar uma entrada costumizada ou mandar sugestões)este é email do meu criador/pai:**famsbussines@gmail.com** ");
+await message.channel.send(" Clica no Link Para mandares email ao meu pai :      https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=CllgCJTNHnzBZPSMhqXjgGzBsRgjCsRXzWKzSrLmkvjHFLWctDWZNlMRpkftCKcsnjNBfrDTKPg                                                                                     Email:**famsbussines@gmail.com** ");
 }
