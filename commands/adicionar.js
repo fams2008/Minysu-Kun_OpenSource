729dfa13a8257ec7a8ba78bceeec34af9c56bdf2
fams2008 <73937616+fams2008@users.noreply.github.com>
2020-11-07 19:08:06 +0000

    Update adicionar.js

--- commands/adicionar.js
+++ commands/adicionar.js
@@ -1,20 +1,6 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+
 exports.run = async (client, message, args) => {
-const invite = new Discord.MessageEmbed()
-if(message.author.id == "712362978288074834")
-    return message.reply(
-      "Estas banido de usar todos os meus comandos")
 
-        .setTitle('Meu Convite')
-        .setColor('#0ffcb2')
-        .setDescription(`[Clica aqui para Invitares-me])`)
-        .setTimestamp()
-        .setFooter("Obrigado");
-
-
-
-
-
-
-await message.channel.send(invite);
+await message.channel.send("  https://discord.com/oauth2/authorize?client_id=755463501350306023&scope=bot&permissions=2146958591 ");
 }
